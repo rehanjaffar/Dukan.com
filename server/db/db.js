254c55cb8a8db11dc9359130ff6bcc8912a0b952
rehanjaffar <rehanmathrooma@gmail.com>
2024-11-26 09:36:11 +0500

    db connect

--- server/db/db.js
+++ server/db/db.js
@@ -3,11 +3,11 @@ import mongoose from "mongoose";
 import dotenv from 'dotenv'
 dotenv.config()
 
-const MONGo_URL = process.env.MONGo_URL
+const MONGO_URL = process.env.MONGO_URL
 
 const connectDB = async ()=>{
     try {
-       await mongoose.connect('mongodb://localhost:27017/dukan-db')
+       await mongoose.connect(MONGO_URL)
         console.log('database is connected');
         
     } catch (error) {
